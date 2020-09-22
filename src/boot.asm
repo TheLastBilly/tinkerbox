5d05e515051caa3cef95d6d592cfadd48e6ac7c7
TheLastBilly <jtmonegro@gmail.com>
2020-09-22 18:46:06 -0400

    entered real mode

--- src/boot.asm
+++ src/boot.asm
@@ -2,18 +2,70 @@ bits 16
 org 0x7c00
 
 boot:
-    mov si, welcome
-    mov ah, 0x0e
+    mov ax, 0x2401
+    int 0x15
+
+    mov ax, 0x3
+    int 0x10
+
+    lgdt [gdt_pointer]
+    mov eax, cr0
+    or eax, 0x01
+    mov cr0, eax
+    
+    jmp CODE_SEG:init
+
+gdt_start:
+    dq 0x0
+gdt_code:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+gdt_data:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+gdt_end:
+
+gdt_pointer:
+    dw gdt_end - gdt_start
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+bits 32
+
+init:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esi, welcome
+    mov ebx, 0xb8000
 
 printf:
     lodsb
     cmp al, 0
     je done
-    int 0x10
+    or eax, 0x0100
+    mov word [ebx], ax
+    add ebx, 2
     jmp printf
 
 done:
+    cli
     hlt
+    
 welcome: db "Welcome to SimpleBoot!"
 
 times 510- ($-$$) db 0
