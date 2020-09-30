c9b96b0cc989d7410ad4d0545efb61754c422164
TheLastBilly <jtmonegro@gmail.com>
2020-09-30 02:13:53 -0400

    added itoa and itoa

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -5,11 +5,15 @@
 #include <kernel/include/x86/terminal.h>
 
 #include <libc/include/string.h>
+#include <libc/include/stdio.h>
 
-char * intro = "Tinkerbox is on the way!";
+char * intro = "Tinkerbox is on the way!\n";
 
 void kmain()
 {
     k_text_mode_print(intro);
+    char buffer[10] = {0};
+    itoa(1234, buffer, 10);
+    k_text_mode_print(buffer);
     return;
 }
--- libc/src/stdio.c
+++ libc/src/stdio.c
@@ -1,10 +1,41 @@
 #include <libc/include/stdio.h>
 
 #include <libc/include/string.h>
+#include <libc/include/stdbool.h>
+
+static char *nitoa(int value, char * str, int base, size_t size)
+{
+    bool is_n = false;
+    if(value < 0)
+    {
+        value *= -1;
+        is_n = true;
+    }
+
+    if(base < 0)
+        base *= -1;
+
+    if(base > 16)
+        return NULL;
+
+    size_t counter = 0;    
+    do
+    {
+        char c = value % base;
+        str[counter++] = c > 9 ? c + 87 : c + 48;
+    }while((value /= base) > 0 && counter < size);
+
+    if(is_n)
+        str[counter] = '-';
+    
+    strrev(str);
+
+    return str;
+}
 
 char * itoa(int value, char * str, int base)
 {
-
+    return nitoa(value, str, base, 50);
 }
 
 void vsnprintf(char * dest, size_t size, const char * fmt, va_list va)
